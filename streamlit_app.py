49e811e9a72456f984ff17ccaf996593216b1905
Vijayshankar-snowflake <125482497+Vijayshankar-snowflake@users.noreply.github.com>
2023-02-16 12:07:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom''s new Healthy dinner')
+streamlit.title('My Mom\'s new Healthy dinner')
 streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
